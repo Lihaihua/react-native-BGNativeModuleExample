1c8150a6a04124fade3bd263f5a12a138da746ab
Lee <haih1983@hotmail.com>
2018-06-27 14:53:41 +0800

    1.0.1
    1.0.1

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import React, { NativeModules } from 'react-native';
 
-module.exports = NativeModules.BGNativeModuleExample;
+//module.exports = NativeModules.BGNativeModuleExample;
+const BGNativeModuleExample = NativeModules.BGNativeModuleExample;
+
+function testPrint(){
+	return BGNativeModuleExample.testPrint();
+} 
+
+export {testPrint}
